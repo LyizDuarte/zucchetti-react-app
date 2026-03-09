dfd7d2a71165169edea7c461b0b3f73115f219e8
LyizDuarte <looduarte21@gmail.com>
2026-03-09 10:35:12 -0300

    feat: UsersPage layout

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,21 +1,63 @@
+import {
+  Alert,
+  CircularProgress,
+  Container,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
 import { useUsers } from '../context/UsersContext';
 
 export function UsersPage() {
   const { users, loading, error } = useUsers();
 
-  if (loading) return <div>Carregando...</div>;
-  if (error) return <div>{error}</div>;
+  if (loading) {
+    return (
+      <Container sx={{ display: 'flex', justifyContent: 'center', mt: 8 }}>
+        <CircularProgress />
+      </Container>
+    );
+  }
+
+  if (error) {
+    return (
+      <Container sx={{ mt: 4 }}>
+        <Alert severity="error">{error}</Alert>
+      </Container>
+    );
+  }
 
   return (
-    <div>
-      <h1>Painel de Usuários</h1>
-      <ul>
-        {users.map(u => (
-          <li key={u.id}>
-            {u.name} - {u.email}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Container sx={{ mt: 4 }}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Painel de Usuários
+      </Typography>
+
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Nome</TableCell>
+              <TableCell>E-mail</TableCell>
+              <TableCell>Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map(user => (
+              <TableRow key={user.id}>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>{user.status}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 }
